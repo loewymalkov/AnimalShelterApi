431a8d2bae98aad577cd34122f03552c4afd9cc1
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-11-01 12:30:55 -0700

    add ApiHelper methods

---
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace AnimalShelterClient.Models
+{
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall()
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/animals");
+            RestRequest request = new RestRequest("/", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallAnimalList(string animals)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/animals");
+            RestRequest request = new RestRequest($"?animalList={animals}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiPost(Word animal)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/animals");
+            RestRequest request = new RestRequest("/", Method.POST);
+            request.AddJsonBody(animal);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiPut(int id, Word animal)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/animals");
+            RestRequest request = new RestRequest($"/{id}", Method.PUT);
+            request.AddJsonBody(animal);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
